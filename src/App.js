1836cd16a22b666d358100b944b61f9acd79ed40
Franklyn Sierra Contreras <franklynssierracontreras@gmail.com>
2024-06-26 13:00:32 -0400

    hello kitty

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Family from './components/Family';
 import Footer from './components/Footer';
 import TopBar from './components/TopBar';
 
-function App() {
+function App() { 
   return (
     <div className="App">
     <TopBar/>
