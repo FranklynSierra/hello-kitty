f7a6c670324edd7b1c4fafb2d454ee2e1c60c4f3
Franklyn Sierra Contreras <franklynssierracontreras@gmail.com>
2024-06-26 14:58:17 -0400

    hello kitty

---
+++ src/components/Origin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Origin() {
+  return (
+    <div className='principalOrigin'id='plataforms'>
+    <div className='origin'>
+     <div className='textorigin'>
+      <h2>Origin</h2>
+      <p>It was designed by Yuko Shimizu and the first product was launched in Japan in 1974 and in the United States in 1976.2​3​ After the first design made by Shimizu, Yuko Yamaguchi became the official designer of Hello Kitty and has been there for more than twenty years. years designing all kinds of Hello Kitty products, accessories and complements.</p>
+      </div>
+     
+        <img className='originImg' src='https://freepngtransparent.com/wp-content/uploads/2023/03/Hello-Kitty-Png-247.png' alt='kitty'/>
+       
+    </div>
+    </div>
+  )
+}
